ecb7af36ae5a788645938d6667e2c0c2dc14321d
g16922kc <g16922kc@s30406.tsuda.ac.jp>
2018-04-12 13:53:48 +0900

    20180412

---
+++ Kadai1/src/Sample1.java
@@ -0,0 +1,7 @@
+
+public class Sample1 {
+	    public static void main(String[] args) {
+	    	System.out.println("プログラミングはあまり得意ではないのですが、");
+	    	System.out.println("今までに習ったことをよく思い出して頑張ります。");
+	}
+}
